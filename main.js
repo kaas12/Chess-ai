5a840129d34ce0e334a1713f9073a5fec4217d4d
kaas12 <37067141+kaas12@users.noreply.github.com>
2021-05-19 16:19:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,38 +3,32 @@ function setup(){
     createCanvas(800, 800);
     }
 function draw(){
-    
+    draw_piece(1 , 1,1)
             //setting up the board
-            
-            
     for (i2 = 0; i2 < 8; i2++){
-            valy = i2;    
+            valy = i2+ 1;    
         for (i = 0; i < 8; i++){
-            valx = i;
+            valx = i + 1;
 
-                if (Number.isInteger((valy + valx)/2)){
-                    sq_color = "black";
+                if (Number.isInteger((valx+valy)/2)){
+                    sq_color = 255;
                     }
                 
                 else{
-                    sq_color = "white";
+                    sq_color = 0;
                 }
             create_square(sq_color, valx,valy)
         }
-        }
-        } 
-
-
+    }
+} 
 function create_square (col, x, y){
     xpos = x * 100;
     ypos = y * 100;
-    if (col == "black"){
-        color_code = 0
-    }
-    else{
-        color_code = 255
-    }
+    fill(col)
     rect(xpos,ypos, 100, 100,)
-    fill(color_code)
-    
+}
+function draw_piece(sort, place){
+    if (sort == 1){
+        console.log("king", place)
+    } 
 }
